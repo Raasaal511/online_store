2088b74cb7113b382a3d5723484656cbe2a11598
Raasaal511 <raasaal511@gmail.com>
2023-11-28 14:10:56 +0300

    First commit

---
+++ backend/products/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=155)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-pk']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=155)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_photos/')
+
+    def __str__(self):
+        return self.image.url
+
+
+class Cart(models.Model):
+    owner = models.OneToOneField(to='users.User', on_delete=models.CASCADE)
+
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    ordering = models.BooleanField(default=True)
