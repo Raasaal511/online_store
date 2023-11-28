73d7141553f6927178419a6fd062d5bf84122d85
Raasaal511 <raasaal511@gmail.com>
2023-11-28 22:15:07 +0300

    add in url simple jwt token

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +10,6 @@ urlpatterns = [
     path('api/v1/users/', include('users.urls')),
     path('api/v1/products/', include('products.urls')),
 
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
