d89f9fd170eef998af181346cb2fd2ef23838dd2
rasal <raasaal511@gmail.com>
2023-12-06 23:12:29 +0300

    Added in admin panel

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Category, Photo, Cart, CartProduct
+
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Photo)
+admin.site.register(Cart)
+admin.site.register(CartProduct)
