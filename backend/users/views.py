aa60a68877e14322e43765621affac97c622ae9f
rasal <raasaal511@gmail.com>
2023-12-06 23:21:49 +0300

    Create user and profile api views.

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import User, Profile
+from .serializers import UserSerializer, UserRegisterSerializer, ProfileSerializer
+
+
+class UserAPIView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        user_data = {
+            'username': serializer.data['username'],
+            'email': serializer.data['email'],
+        }
+        return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+class ProfileAPIView(APIView):
+    def get(self, request):
+        profile = Profile.objects.get(user_id=request.user.id)
+        serializer = ProfileSerializer(profile)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
