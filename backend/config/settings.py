35ea4f28304b9a0be0e187b72fb8b317128463db
rasal <raasaal511@gmail.com>
2023-12-06 23:19:29 +0300

    Change authentication classes from JWT to session

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -65,9 +65,9 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
 
 WSGI_APPLICATION = 'config.wsgi.application'
