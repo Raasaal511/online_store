6a62afe381141c66ac06df38980513a1ed19797b
rasal <raasaal511@gmail.com>
2023-12-11 11:51:59 +0300

    Create for admin panel inline model photo for model product.

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 from .models import Product, Category, Photo, Cart, CartProduct
 
 
-admin.site.register(Product)
-admin.site.register(Category)
+class PhotoInline(admin.StackedInline):
+    model = Photo
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [PhotoInline]
+
+
 admin.site.register(Photo)
+admin.site.register(Category)
 admin.site.register(Cart)
 admin.site.register(CartProduct)
