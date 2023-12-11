194ada0731b9ec8f6fab05fc32d0609a1c716a37
rasal <raasaal511@gmail.com>
2023-12-11 11:54:53 +0300

    Added save method for product and category model.

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,26 +1,39 @@
 from django.db import models
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=155)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        return super().save(args, kwargs)
 
 
 class Product(models.Model):
     name = models.CharField(max_length=155)
     description = models.TextField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        return super().save(args, kwargs)
 
     class Meta:
         ordering = ['-pk']
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=155)
-    slug = models.SlugField(unique=True)
-
-    def __str__(self):
-        return self.name
 
 
 class Photo(models.Model):
