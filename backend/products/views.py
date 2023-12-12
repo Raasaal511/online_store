dac5bb1467fd82b569c44731beb5031a5fe64efe
rasal <raasaal511@gmail.com>
2023-12-12 12:35:54 +0300

    Added viewsets for product, category, cartproduct models.

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Product, Category, CartProduct
+from .serializers import ProductSerializer, CategorySerializer, CartProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'slug'
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
+
+
+class CartProductViewSet(viewsets.ModelViewSet):
+    queryset = CartProduct.objects.all()
+    serializer_class = CartProductSerializer
+    lookup_field = 'id'
+
