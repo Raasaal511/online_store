48b7333bd4a9aa0010b01785ad85f868766dbdd1
rasal <raasaal511@gmail.com>
2024-01-27 02:06:33 +0300

    test ver. will be changed in the future

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -4,33 +4,12 @@ from rest_framework_simplejwt.serializers import PasswordField
 from .models import User, Profile
 
 
-class UserRegisterSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     password = PasswordField()
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            phone_mobile=validated_data.get('phone_mobile', None)
-        )
-        return user
-
-
-class UserLoginSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('email', 'password')
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'first_name', 'last_name')
 
 
 class ProfileSerializer(serializers.ModelSerializer):
