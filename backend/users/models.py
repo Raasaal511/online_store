47d506eee7a247e2037f2aa2c98efa463cdf6599
rasal <raasaal511@gmail.com>
2024-01-26 12:59:27 +0300

    Added required fields to the user model.

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,7 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    phone_mobile = models.CharField(max_length=155, null=True, blank=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    username = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=155, null=True, blank=True)
+
+    def __str__(self):
+        return self.email
 
 
 class Profile(models.Model):
