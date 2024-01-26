371f554485c4bff72c661bab2cf84d814b89f1cc
rasal <raasaal511@gmail.com>
2024-01-27 02:09:32 +0300

    added djoser and token urls.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView
 )
@@ -9,9 +12,17 @@ urlpatterns = [
 
     path('api/auth/', include('rest_framework.urls')),
 
-    path('api/v1/users/', include('users.urls')),
+    # path('api/v1/users/', include('users.urls')),
     path('api/v1/products/', include('products.urls')),
 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(r'api/v1/auth/', include('djoser.urls')),
+    path(r'api/v1/auth/', include('djoser.urls.jwt')),
+
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
