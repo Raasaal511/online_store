e03e75171b4f0dbddabf5604a3527e618e04b6f1
rasal <raasaal511@gmail.com>
2024-01-27 02:17:15 +0300

    update cart product, set up a list method to view data prices.

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from django.db.models import Sum, F
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from .models import Product, Category, CartProduct
 from .serializers import ProductSerializer, CategorySerializer, CartProductSerializer
@@ -21,3 +23,19 @@ class CartProductViewSet(viewsets.ModelViewSet):
     serializer_class = CartProductSerializer
     lookup_field = 'id'
 
+    def list(self, request, *args, **kwargs):
+        # Умножает сумму продукта на количество в карзине  и фильтрует его по пользователю
+        cart_products = CartProduct.objects.annotate(
+            total_product_price=Sum(F('product__price') * F('quantity')),
+        ).filter(cart__owner=self.request.user.id)
+
+        # Получает всю сумму товаров от продуктов в корзине
+        cart_total_price = cart_products.filter(ordering=True).aggregate(
+            total_cost_cart=Sum('total_product_price'))
+
+        serializer = CartProductSerializer(cart_products, many=True)
+
+        return Response({
+            'products': serializer.data,
+            'cart_total': cart_total_price
+            }, status=status.HTTP_200_OK)
