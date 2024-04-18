b6fe45ee7c4800d0527f22b728145aa4e9907207
rasal <raasaal511@gmail.com>
2024-04-18 16:01:09 +0300

    ProductViewSet added filter_backend and update filter CartProductViewSet

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,15 +1,17 @@
+import django_filters
 from django.db.models import Sum, F
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
-from .models import Product, Category, CartProduct
-from .serializers import ProductSerializer, CategorySerializer, CartProductSerializer
+from .models import Product, Category, CartProduct, Photo
+from .serializers import ProductSerializer, CategorySerializer, CartProductSerializer, PhotoSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'slug'
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -18,16 +20,22 @@ class CategoryViewSet(viewsets.ModelViewSet):
     lookup_field = 'slug'
 
 
+# Надо посмотреть что делать с veiw моделью Photo
+class PhotoViewSet(viewsets.ModelViewSet):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+
+
 class CartProductViewSet(viewsets.ModelViewSet):
     queryset = CartProduct.objects.all()
     serializer_class = CartProductSerializer
     lookup_field = 'id'
 
     def list(self, request, *args, **kwargs):
+        cart_products = CartProduct.objects.all().filter(cart__owner=self.request.user.id)
         # Умножает сумму продукта на количество в карзине  и фильтрует его по пользователю
-        cart_products = CartProduct.objects.annotate(
-            total_product_price=Sum(F('product__price') * F('quantity')),
-        ).filter(cart__owner=self.request.user.id)
+        cart_products = cart_products.annotate(
+            total_product_price=Sum(F('product__price') * F('quantity')))
 
         # Получает всю сумму товаров от продуктов в корзине
         cart_total_price = cart_products.filter(ordering=True).aggregate(
@@ -37,5 +45,5 @@ class CartProductViewSet(viewsets.ModelViewSet):
 
         return Response({
             'products': serializer.data,
-            'cart_total': cart_total_price
-            }, status=status.HTTP_200_OK)
+            'total_cost_cart': cart_total_price['total_cost_cart'] if cart_total_price['total_cost_cart'] else 0
+        }, status=status.HTTP_200_OK)
