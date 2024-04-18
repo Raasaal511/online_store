65c2ef9cdbf1b3171f03efe4c85aa7a67360e6a1
rasal <raasaal511@gmail.com>
2024-04-18 15:53:13 +0300

    replace save method on AutoSlugField

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,45 +1,53 @@
 from django.db import models
-from django.utils.text import slugify
+from django_extensions.db.fields import AutoSlugField
 
 
 class Category(models.Model):
     name = models.CharField(max_length=155)
-    slug = models.SlugField(unique=True, blank=True, null=True)
+    slug = AutoSlugField(unique=True, blank=True, null=True, populate_from='name')
 
     def __str__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-
-        return super().save(*args, **kwargs)
 
 
 class Product(models.Model):
     name = models.CharField(max_length=155)
     description = models.TextField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    slug = models.SlugField(unique=True, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 related_name='category')
+    slug = AutoSlugField(unique=True, blank=True, null=True, populate_from='name')
 
     def __str__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-
-        return super().save(*args, **kwargs)
 
     class Meta:
         ordering = ['-pk']
 
 
+# class BaseImage(models.Model):
+#     """Basic model for images"""
+#     title = models.CharField(max_length=200, null=True, blank=True)
+#     alt = models.CharField(max_length=200, null=True, blank=True)
+#     image = VersatileImageField(null=True, blank=True, upload_to='images')
+#
+#     class Meta:
+#         abstract = True
+#         verbose_name = "Image"
+#         verbose_name_plural = "Images"
+
+    # def __str__(self):
+    #     res = ''
+    #     if self.title:
+    #         res = self.title
+    #     else:
+    #         res = self.image.url
+    #     return res
+
 class Photo(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE,
-                                related_name='photos', null=True, blank=True)
-    image = models.ImageField(upload_to='product/photos/', null=True, blank=True)
+                                related_name='photos')
+    image = models.ImageField(upload_to='product/photos/', default='product/default.png', null=True, blank=True)
 
     def __str__(self):
         return self.image.url
